7c1e7adb0f6ac27042386ed1c41323a5820f3149
Cláudio Mindouro <claudiopedrom@gmail.com>
2020-09-03 22:14:29 +0100

    feat(styles): add pages styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,19 @@ import Head from 'next/head'
 
 import RocketseatLogo from '../assets/rocketseat.svg'
 
+import { Container } from '../styles/pages/Home'
+
 const Home: React.FC = () => {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Homepage</title>
       </Head>
 
-      <main>
-        <RocketseatLogo />
-        <h1>Hello World</h1>
-      </main>
-    </div>
+      <RocketseatLogo />
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + Next.js structure made by Rocketseat.</p>
+    </Container>
   )
 }
 
